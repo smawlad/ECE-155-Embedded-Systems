e464f59923cd23671cf9d23150c778f4b02c1a14
jasonkwok <jasonzakskwok@gmail.com>
2017-01-24 16:41:49 -0500

    Lab 2 Files

---
+++ Lab2_201_06/app/src/main/java/lab2_201_06/uwaterloo/ca/lab2_201_06/MainActivity.java
@@ -0,0 +1,13 @@
+package lab2_201_06.uwaterloo.ca.lab2_201_06;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
